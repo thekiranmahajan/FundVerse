96ddf16ad7237d805b6a1a4c9737f1f311eed5ce
Kiran Mahajan <91893931+thekiranmahajan@users.noreply.github.com>
2023-09-24 21:28:51 +0530

    integreted Sepolia test network

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -33,6 +33,14 @@ module.exports = {
   },
   solidity: {
     version: "0.8.17",
+    defaultNetwork: "sepolia",
+    networks: {
+      hardhat: {},
+      sepolia: {
+        url: "https://sepolia.rpc.thirdweb.com",
+        accounts: [`0x${process.env.PRIVATE_KEY}`],
+      },
+    },
     settings: {
       optimizer: {
         enabled: true,
