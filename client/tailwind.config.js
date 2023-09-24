3c18d8cd27f82173932429c95a3c639077bba957
Kiran Mahajan <91893931+thekiranmahajan@users.noreply.github.com>
2023-09-24 23:07:25 +0530

    added custom font family & boxShadow

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        epilogue: ["Epilogue", "sans-serif"],
+      },
+      boxShadow: {
+        secondary: "10px 10px 20px rgba(2, 2, 2, 0.25)",
+      },
+    },
   },
   plugins: [],
 };
