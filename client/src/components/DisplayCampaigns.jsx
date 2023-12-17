95b99fec2db7be8d1de1886a7c0d78d20d21c21f
Kiran Mahajan <91893931+thekiranmahajan@users.noreply.github.com>
2023-12-17 23:16:57 +0530

    component created to show campaigns made by owner

---
+++ client/src/components/DisplayCampaigns.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { loader } from "../assets";
+import FundCard from "./FundCard";
+import { v4 as uuidv4 } from "uuid";
+
+const DisplayCampaigns = ({ title, isloading, campaigns }) => {
+  const navigate = useNavigate();
+
+  const handleNavigate = (campaign) => {
+    navigate(`/campain-details/${campaign.title}`, { state: campaign });
+  };
+
+  return (
+    <div>
+      <h1 className="font-epilogue font-semibold text[18px] text-white text-left">
+        {title} ({campaigns.length})
+        <div className="flex flex-wrap mt-[20px] gap-[26px] ">
+          {isloading && (
+            <img
+              src={loader}
+              alt="loader"
+              className="w-[100px] h-[100px] object-contain "
+            />
+          )}
+          {!isloading && campaigns.length === 0 && (
+            <p className="font-epilogue font-semibold text-[14px] leading-[30px] text[#818183] ">
+              You have not created any campaigns yet
+            </p>
+          )}
+
+          {!isloading &&
+            campaigns.length > 0 &&
+            campaigns.map((campaign) => (
+              <FundCard
+                key={uuidv4}
+                {...campaign}
+                handleClick={() => handleNavigate(campaign)}
+              />
+            ))}
+        </div>
+      </h1>
+    </div>
+  );
+};
+
+export default DisplayCampaigns;
