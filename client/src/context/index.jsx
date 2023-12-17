e14a64b29d76c53bc7b88ecee8a0ddce50da34c9
Sakshi Jaiswal <sakshijaiswal0211@gmail.com>
2023-12-17 16:37:22 +0530

    added function named parsedCampaign

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -42,7 +42,18 @@ export const StateContextProvider = ({ children }) => {
   const getCampaigns = async () =>{
     const campaigns = await contract.call('getCampaigns');
 
-    console.log(campaigns)
+    const parsedCampaigns = campaigns.map((campaign, i)=>({
+      owner: campaign.owner,
+      title: campaign.title,
+      description: campaign.description,
+      target: ethers.utils.formatEther(campaign.target.toString()),
+      deadline: campaign.deadline.toNumber(),
+      amountCollected: ethers.utils.formatEther(campaign.amountCollected.toString()),
+      image: campaign.image,
+      pId: i
+
+    }));
+    return parsedCampaigns;
   }
   return (
     <StateContext.Provider
