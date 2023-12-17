4c86865dcd2165ad80d0f7931d22904bb0157e12
Sakshi Jaiswal <sakshijaiswal0211@gmail.com>
2023-12-17 15:58:42 +0530

    added required useStates in Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React,{ useState, useEffect } from 'react'
+
+import { useStateContext } from '../context'
 
 const Home = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [campaigns, setCampaigns] = useState([]);
+  const { address, contract, getCampaigns } = useStateContext();
   return (
     <div>Home</div>
   )
