b5cfe388768f39120d9daea2c810e080939f16dd
Kiran Mahajan <91893931+thekiranmahajan@users.noreply.github.com>
2023-12-27 13:22:30 +0530

    isLoading in DisplayCampaign component fixed✅

--- client/src/components/DisplayCampaigns.jsx
+++ client/src/components/DisplayCampaigns.jsx
@@ -4,7 +4,7 @@ import { loader } from "../assets";
 import FundCard from "./FundCard";
 import { v4 as uuidv4 } from "uuid";
 
-const DisplayCampaigns = ({ title, isloading, campaigns }) => {
+const DisplayCampaigns = ({ title, isLoading, campaigns }) => {
   const navigate = useNavigate();
 
   const handleNavigate = (campaign) => {
@@ -16,20 +16,20 @@ const DisplayCampaigns = ({ title, isloading, campaigns }) => {
       <h1 className="font-epilogue font-semibold text[18px] text-white text-left">
         {title} ({campaigns.length})
         <div className="flex flex-wrap mt-[20px] gap-[26px] ">
-          {isloading && (
+          {isLoading && (
             <img
               src={loader}
               alt="loader"
               className="w-[100px] h-[100px] object-contain "
             />
           )}
-          {!isloading && campaigns.length === 0 && (
+          {!isLoading && campaigns.length === 0 && (
             <p className="font-epilogue font-semibold text-[14px] leading-[30px] text[#818183] ">
               You have not created any campaigns yet
             </p>
           )}
 
-          {!isloading &&
+          {!isLoading &&
             campaigns.length > 0 &&
             campaigns.map((campaign) => (
               <FundCard
