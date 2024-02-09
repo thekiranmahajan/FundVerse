d295132b6112104f504699349961c64771116daa
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-02-09 10:00:03 +0530

    added style customisation for scrollbar and arrows of number input using Tailwind CSS layer directives

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+import defaultTheme from "tailwindcss/defaultTheme";
+export default {
+  darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        epilogue: ["Epilogue", "sans-serif"],
+        epilogue: ["Epilogue", ...defaultTheme.fontFamily.serif],
       },
       boxShadow: {
         secondary: "10px 10px 20px rgba(2, 2, 2, 0.25)",
