451cbe63d344d2aa51b73dde860ad9f9b969155d
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-02-11 22:17:47 +0530

    Modified Withdraw removed heading

--- client/src/pages/Withdraw.jsx
+++ client/src/pages/Withdraw.jsx
@@ -20,20 +20,17 @@ const Withdraw = () => {
   }, [address, contract]);
 
   return (
-    <div className="bg-[#1c1c24] flex items-center  flex-col rounded-[10px] sm:p-10 p-4 ">
-      <div className="flex items-center justify-center p-[16px] sm:min-w-[380px] bg-[#3a3a43] rounded-[10px] ">
-        <h1 className=" font-epilogue font-bold sm:text-[25px] text-[18px] leading-[38px] text-white ">
-          Withdraw
-        </h1>
+    <>
+      <div>
+        <div className=" w-full mt-8 flex flex-col gap-[30px] ">
+          <WithdrawFromCampaigns
+            title="My Campaigns"
+            isLoading={isLoading}
+            campaigns={campaigns}
+          />
+        </div>
       </div>
-      <div className=" w-full mt-8 flex flex-col gap-[30px] ">
-        <WithdrawFromCampaigns
-          title="My Campaigns"
-          isLoading={isLoading}
-          campaigns={campaigns}
-        />
-      </div>
-    </div>
+    </>
   );
 };
 
