9e2ad30cedb846c2ea133ccf10fb6cfbaa98004f
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-02-12 15:58:41 +0530

    Implemented on hover tooltip

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -3,31 +3,48 @@ import { Link, useNavigate } from "react-router-dom";
 import { logo, sun } from "../assets";
 import { navlinks } from "../constants";
 
-const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => (
-  <div
-    className={`w-[48px] h-[48px] rounded-[10px] ${
-      isActive && isActive === name && "bg-[#2c2f32]"
-    } flex justify-center items-center ${
-      !disabled && "cursor-pointer"
-    } ${styles}`}
-    onClick={handleClick}
-  >
-    {!isActive ? (
-      <img src={imgUrl} alt="fund_logo" className="w-1/2 h-1/2" />
-    ) : (
-      <img
-        src={imgUrl}
-        alt="fund_logo"
-        className={`w-1/2 h-1/2 ${isActive !== name && "grayscale"}`}
-      />
-    )}
-  </div>
-);
+const Icon = ({
+  styles,
+  name = "Home",
+  imgUrl,
+  isActive,
+  disabled,
+  handleClick,
+}) => {
+  const [hovered, setHovered] = useState(false);
+
+  return (
+    <div
+      className={`relative w-[48px] h-[48px] rounded-[10px] ${
+        isActive && isActive === name && "bg-[#2c2f32]"
+      } flex justify-center items-center ${
+        !disabled && "cursor-pointer"
+      } ${styles}`}
+      onClick={handleClick}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+    >
+      {hovered && (
+        <div className="absolute top-1 left-20 bg-[#2c2f32] text-white py-2 px-3 rounded-md font-epilogue">
+          {name}
+        </div>
+      )}
+      {!isActive ? (
+        <img src={imgUrl} alt="fund_logo" className="w-1/2 h-1/2" />
+      ) : (
+        <img
+          src={imgUrl}
+          alt="fund_logo"
+          className={`w-1/2 h-1/2 ${isActive !== name && "grayscale"}`}
+        />
+      )}
+    </div>
+  );
+};
 
 const Sidebar = () => {
   const navigate = useNavigate();
   const [isActive, setIsActive] = useState("dashboard");
-
   return (
     <div className="flex justify-between items-center flex-col sticky top-5 h-[93vh]">
       <Link to="/">
