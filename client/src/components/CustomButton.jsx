998edfb19a78679875f2dece8b50f12b559e6ec5
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-02-13 11:18:28 +0530

    Added animation to button introduced the new called isDisabled

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 
-const CustomButton = ({ btnType, title, handleClick, styles }) => {
+const CustomButton = ({
+  btnType,
+  title,
+  handleClick,
+  styles,
+  isDisabled = false,
+}) => {
+  console.log(isDisabled);
   return (
     <button
       type={btnType}
-      className={`font-epilogue font-semibold text-[16px] leading-[26px] text-white min-h-[52px] px-4 rounded-[10px] ${styles}`}
+      className={`font-epilogue font-semibold text-[16px] leading-[26px] text-white min-h-[52px] px-4 rounded-[10px] transition-all duration-300 hover:scale-95 focus:scale-105 outline-none focus:ring-4 ${styles} ${
+        isDisabled && "bg-gray-500 cursor-not-allowed"
+      }`}
+      disabled={isDisabled}
       onClick={handleClick}
     >
       {title}
