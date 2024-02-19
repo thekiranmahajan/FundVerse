dcb44a00a4812ce9d773adfbf4dc9551298bf8f5
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-02-19 22:13:36 +0530

    Properly configured the dotenv variable for accessing API key

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,13 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
+import dotenv from "dotenv";
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
   define: {
-    "process.env": {},
+    "process.env": process.env,
   },
 });
