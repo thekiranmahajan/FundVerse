876487cd85c2eb52f8db1213be0fb71d693d5ebb
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-02-29 01:42:57 +0530

    Rendered FundCards inside DisplayCampaigns with new central campaigns and remove state obj added optional chaining to avoid errors

--- client/src/components/DisplayCampaigns.jsx
+++ client/src/components/DisplayCampaigns.jsx
@@ -7,16 +7,14 @@ import { v4 as uuidv4 } from "uuid";
 const DisplayCampaigns = ({ title, isLoading, campaigns }) => {
   const navigate = useNavigate();
 
-  const handleNavigate = (campaign) => {
+  const handleNavigateDetails = (campaign) => {
     console.log("campaigns from DisplayCampaigns", campaign);
-    navigate(`/campaign-details/${encodeURIComponent(campaign.id)}`, {
-      state: campaign,
-    });
+    navigate(`/campaign-details/${campaign.id}`);
   };
   return (
     <div>
       <h1 className="font-epilogue font-semibold text-lg text-black dark:text-white text-left">
-        {title} ({campaigns.length})
+        {title} ({campaigns?.length})
       </h1>
       <div className="flex flex-wrap mt-[20px] gap-[26px] ">
         {isLoading && (
@@ -26,21 +24,23 @@ const DisplayCampaigns = ({ title, isLoading, campaigns }) => {
             className="w-[100px] h-[100px] object-contain "
           />
         )}
-        {!isLoading && campaigns.length === 0 && (
+        {!isLoading && campaigns?.length === 0 && (
           <p className="font-epilogue font-semibold text-sm leading-8 text-[#4e4e4e] dark:text-[#818183] ">
             You have not created any campaigns yet
           </p>
         )}
 
-        {!isLoading &&
-          campaigns.length > 0 &&
-          campaigns.map((campaign) => (
-            <FundCard
-              key={uuidv4()}
-              {...campaign}
-              handleClick={() => handleNavigate(campaign)}
-            />
-          ))}
+        {!isLoading && campaigns?.length > 0 && (
+          <>
+            {campaigns.map((campaign) => (
+              <FundCard
+                key={uuidv4()}
+                {...campaign}
+                handleClick={() => handleNavigateDetails(campaign)}
+              />
+            ))}
+          </>
+        )}
       </div>
     </div>
   );
